9193229cb31cc2f8ff1d510ad1a3fb0c19b0883b
Roberto <robertjim12345@gmail.com>
2026-06-08 03:26:13 -0600

    cambia handleinputchange por handlechange

--- app/dashboard/perfil/page.tsx
+++ app/dashboard/perfil/page.tsx
@@ -67,7 +67,9 @@ export default function EditarPerfilPage() {
   }, [user]);
 
   const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
   ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
