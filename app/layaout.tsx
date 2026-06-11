10c95285221450408b72206518f52db2faf0c18a
Roberto <robertjim12345@gmail.com>
2026-06-11 13:02:23 -0600

    actualiza color de fondo de simulador seguro

--- app/layaout.tsx
+++ app/layaout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${inter.variable} font-sans bg-slate-50 text-slate-900 antialiased`}
+        className={`${inter.variable} font-sans bg-slate-900 text-slate-900 antialiased`}
       >
         {children}
       </body>
